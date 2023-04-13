4aae94fa10cdf3644c997dd55e5835a4b8e82221
Shin-sangwon <dnjsml10@gmail.com>
2023-04-13 22:29:01 +0900

    데이터 바인딩 3 -> 왜 해야하는가?

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <div>
+    <h2 class="red" :style="mystyle">원룸샵</h2>
     <h4>XX 원룸</h4>
     <p>{{ price1 }} 만원</p>
   </div>
   <div>
     <h4>XX 원룸</h4>
-    <p>XX 만원</p>
+    <p>{{ price2 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ price2 }} 만원</p>
   </div>
 </template>
 
@@ -14,9 +27,16 @@
 export default {
   name: "App",
   // 데이터 보관 함 부터 만들어야 함
+  // 데이터 바인딩 하는 이유
+  // 1. HTML에 하드코딩해놓으면 나중에 변경이 어려움
+  //★2. VUE의 실시간 자동 렌더링 기능을 사용하기 위해
+  // 3. HTML 속성도 데이터바인딩 가능하다. ({{}} 대신 : 문법 사용해야 함)
   data() {
     return {
       price1: 60,
+      price2: 70,
+      mystyle: "color : blue",
+      products: ["장덕동원룸", "수완동원룸", "첨단원룸"],
     };
   },
   components: {},
